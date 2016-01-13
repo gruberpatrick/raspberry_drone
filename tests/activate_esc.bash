b02b3cf40bd8847ebe7d651407133ee35f9e88e0
Gruber Patrick <patrick19gruber93@gmail.com>
2016-01-13 17:48:32 -0600

    activate esc

---
+++ tests/activate_esc.bash
@@ -0,0 +1,11 @@
+echo "---------------------------------------------------"
+echo " Tie your drone down, otherwise you might loose it!"
+echo "---------------------------------------------------"
+
+read nothing
+
+pigs s $1 1000 # minimum throttle
+sleep 1
+pigs s $1 2000 # maximum throttle
+sleep 1
+pigs s $1 1200 # slightly open throttle
