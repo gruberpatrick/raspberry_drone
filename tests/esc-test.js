b15734ddb425910fd0bff34cba52c2ff2637172c
Patrick Gruber <patrick.gruber@student.tugraz.at>
2016-01-13 16:33:48 -0600

    testing node implementation of pigpio

--- tests/esc-test.js
+++ tests/esc-test.js
@@ -8,16 +8,12 @@ aESC[3] = require("../lib/esc");
 // initialize ESC's
 console.log("[ESC] initializing");
 aESC[0].init(1, parseInt(process.argv[2]));
-aESC[1].init(1, parseInt(process.argv[3]));
-aESC[2].init(1, parseInt(process.argv[4]));
-aESC[3].init(1, parseInt(process.argv[5]));
+aESC[1].init(2, parseInt(process.argv[3]));
+aESC[2].init(3, parseInt(process.argv[4]));
+aESC[3].init(4, parseInt(process.argv[5]));
 
-// set setting to full
-setTimeout(function(){
-  aESC[0].startFull();
-  aESC[1].startFull();
-  aESC[2].startFull();
-  aESC[3].startFull();
-}, 1000);
+while(!aESC[3].bReady){} // wait for last ESC to be activated
 
-while(1){}
+for(var lIndex in aESC) aESC[lIndex].setPWM(1300);
+
+while(1){} // let the program run
