16b551b8291fac11be1b614f048c08534bb1fa97
Gruber Patrick <patrick19gruber93@gmail.com>
2016-01-12 22:35:07 -0600

    adding esc library & first implementations

---
+++ tests/esc-test.js
@@ -0,0 +1,14 @@
+// include test library
+var aESC = [];
+aESC[0] = require("../lib/esc");
+aESC[1] = require("../lib/esc");
+aESC[2] = require("../lib/esc");
+aESC[3] = require("../lib/esc");
+
+// initialize ESC's
+console.log("[ESC] initializing");
+aESC[0].init(1, 35);
+
+setTimeout(aESC[0].startFull, 1000);
+
+while(1){}
