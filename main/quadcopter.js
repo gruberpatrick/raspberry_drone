f0795b4e3c2a8ffb0a4e0752f0b05b007b26f47b
Patrick Gruber <patrick.gruber@student.tugraz.at>
2016-01-14 14:41:48 -0600

    transfer push

--- main/quadcopter.js
+++ main/quadcopter.js
@@ -5,22 +5,27 @@ var oMS5611 = require("../lib/ms5611");
 // include networking components
 var oNetwork = require("../lib/HandyJS/lib/network-p");
 
-// initialize ESC's
-console.log("[QUADCOPTER] initialize");
-oESC.init([9,7,23,19]); // initialize ESC's on given GPIO's
+function Quadcopter(){
 
-// connect to server and identify
-oNetwork.oSocket.connectWebSocket("192.168.1.16", 4444, function(){
-  // connected to server
-  console.log("[QUADCOPTER] connected to server");
-}, function(oLastConnection, lFlags){
-  // message from server
-  console.log("[QUADCOPTER] message from server:\nlFlag -> " + lFlags);
-  console.log(oLastConnection);
-}, function(oErr){
-  // error
-  console.log(oErr);
-});
-oNetwork.oSocket.serverSend(JSON.stringify({init:true,sType:"quadcopter"}));
+  // initialize quadcopter
+  this.initialize = function(){
+    // initialize ESC's
+    console.log("[QUADCOPTER] initialize");
+    oESC.init([9,7,23,19]); // initialize ESC's on given GPIO's
+    // connect to server and identify
+    oNetwork.oSocket.connectWebSocket("192.168.1.16", 4444, function(){
+      // connected to server
+      console.log("[QUADCOPTER] connected to server");
+      oNetwork.oSocket.serverSend(JSON.stringify({init:true,sType:"quadcopter"}));
+    }, function(oLastConnection, lFlags){
+      // message from server
+      console.log("[QUADCOPTER] message from server:\nlFlag -> " + lFlags);
+      console.log(oLastConnection);
+    }, function(oErr){
+      // error
+      console.log(oErr);
+    });
+  };
 
+}
 
