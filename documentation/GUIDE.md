f6ad69a1bc3de74b1a0027e470c7c0a3efbf6319
Patrick Gruber <patrick.gruber@student.tugraz.at>
2016-01-13 18:24:14 -0600

    quadcopter start

---
+++ documentation/GUIDE.md
@@ -0,0 +1,16 @@
+# 1) Hardware INITIALIZATION
+
+Standard ESC's may be hooked up to any Raspberry PI GPIO. Usually this cable is white. Black cables provided by the ESC need to
+be hooked up to a GND (ground) pin. The red (5V) cables are not needed for the PI, as they provide external hardware with power.
+
+
+
+# 2) Software INITIALIZATION
+
+If you haven't initialized your drone for the Raspberry Pi, the ESC's will wait for their initialization. To do so, simply run:
+`bash/activate_esc.bash [GPIO#]`
+This will cause the ESC to stop beeping and is set to the ready state. The 'esc.js' library commands may now follow.
+
+# 3) Library USAGE
+
+[TODO]
---
+++ quadcopter.js
@@ -0,0 +1,23 @@
+// create ESC objects
+var aESC = [];
+aESC[0] = [require("../lib/esc"), 9];
+aESC[1] = [require("../lib/esc"), 7];
+aESC[2] = [require("../lib/esc"), 23];
+aESC[3] = [require("../lib/esc"), 19];
+
+// include sensors
+var oMPU6050 = require("../lib/mpu6050");
+var oMS5611 = require("../lib/ms5611");
+
+// include websocket for external controls
+var oWS = require("ws");
+
+// initialize each ESC
+console.log("[QUADCOPTER] initialize");
+for(var lIndex in aESC){ aESC[lIndex][0].init(lIndex, aESC[lIndex][1]); }
+
+// test ESC's
+for(var lIndex in aESC){ aESC[lIndex][0].setPWM(1240); }
+setTimeout(function(){
+  for(var lIndex in aESC){ aESC[lIndex][0].setPWM(1000); }
+}, 5000);
